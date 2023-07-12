66537ea660e5c81478a9c3733258215051bf868a
anhthu112 <108163330+anhthu112@users.noreply.github.com>
2023-07-13 00:14:35 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function Buy(packageType,price,packagetype,price1){
+    localStorage.setItem('packageType', packageType);  
+    localStorage.setItem('price', price);
+    localStorage.setItem('packagetype', packagetype);  
+    localStorage.setItem('price1', price1);
+
+}
+
+ 
